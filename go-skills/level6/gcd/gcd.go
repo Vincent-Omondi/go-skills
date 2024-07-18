600d4cb70e52ff6b853c759c767a8ddf4d81167b
Vincent-Omondi <vincentomondi251@gmail.com>
2024-07-18 15:37:19 +0300

    update: update gcd function

--- go-skills/level6/gcd/gcd.go
+++ go-skills/level6/gcd/gcd.go
@@ -1,48 +1,17 @@
 package main
 
-import (
-	"os"
+import "fmt"
 
-	"github.com/01-edu/z01"
-)
-
-func gcd(a, b int) int {
+func Gcd(a, b int) int {
 	for b != 0 {
 		a, b = b, a%b
 	}
 	return a
 }
 
-func atoi(s string) int {
-	i := 0
-	for _, n := range s {
-		if n < '0' || n > '9' {
-			return 0
-		}
-		i = i*10 + int(n-'0')
-	}
-	return i
-}
-
 func main() {
-	if len(os.Args) != 3 {
-		return
-	}
-
-	num1, num2 := atoi(os.Args[1]), atoi(os.Args[2])
-
-	res := itoa(gcd(num1, num2))
-
-	for _,r := range res {
-		z01.PrintRune(r)
-	}
-	z01.PrintRune('\n')
-}
-
-func itoa(n int) string {
-	s := ""
-	for ; n > 0; n /= 10 {
-		s = string(byte(n%10+'0')) + s
-	}
-	return s
+	fmt.Println(Gcd(42, 10))
+	fmt.Println(Gcd(42, 12))
+	fmt.Println(Gcd(14, 77))
+	fmt.Println(Gcd(17, 3))
 }
