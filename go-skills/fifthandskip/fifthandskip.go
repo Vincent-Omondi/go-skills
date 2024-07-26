d253d35784e097cd83b4457ee5b944a98ea26538
vinomondi <vincentomondi1002@gmail.com>
2024-07-26 05:57:51 +0300

    chore:update fifthandskip

--- go-skills/fifthandskip/fifthandskip.go
+++ go-skills/fifthandskip/fifthandskip.go
@@ -1,62 +1,42 @@
 package main
 
-import "fmt"
-
-func FifthAndSkip(str string) string {
-	if str == "" {
-		return "\n"
-	}
-
-	// Trim leading spaces
-	for len(str) > 0 && str[0] == ' ' {
-		str = str[1:]
-	}
-
-	// Trim trailing spaces
-	for len(str) > 0 && str[len(str)-1] == ' ' {
-		str = str[:len(str)-1]
-	}
-
-	// Replace multiple internal spaces with a single space
-	var trimmed string
-	inSpace := false
-	for _, ch := range str {
-		if ch == ' ' {
-			if !inSpace {
-				trimmed += ""
-				inSpace = true
-			}
-		} else {
-			trimmed += string(ch)
-			inSpace = false
-		}
-	}
-
-	if len(trimmed) < 5 {
-		return "Invalid Input$\n"
-	}
-
-	var result string
-	for i := 0; i < len(trimmed); i += 5 {
-		endIndex := i + 5
-		if endIndex > len(trimmed) {
-			endIndex = len(trimmed)
-		}
-		substr := trimmed[i:endIndex]
-		if len(substr) >= 6 {
-			substr = substr[:5] + substr[6:]
-		}
-		result += substr
-		if i+5 < len(trimmed) {
-			result += " "
-		}
-	}
-
-	return result + "$\n"
-}
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Print(FifthAndSkip("abcdefghijklmnopqrstuwxyz"))
-	fmt.Print(FifthAndSkip("This  is  a  short  sentence"))
+	fmt.Print(FifthAndSkip("This is a short sentence"))
 	fmt.Print(FifthAndSkip("1234"))
+}
+
+func FifthAndSkip(str string) string {
+	words := ""
+	for _, r := range str {
+		if r == ' ' {
+			continue
+		}
+		words += string(r)
+	}
+
+	if len(words) == 0 {
+		return "Invalid Input\n"
+	}
+
+	res := ""
+	count := 0
+	for i := 0; i < len(words); i++ {
+
+		if count == 5 {
+			count = 0
+			continue
+		}
+		res += string(words[i])
+		count++
+
+		if count == 5 && i+1 < len(words) {
+			res += " "
+		}
+	}
+	return res + "\n"
 }
