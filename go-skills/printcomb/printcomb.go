9180e12884b9b6f11ec6e5e34fae3afcff9937b0
vinomondi <vincentomondi1002@gmail.com>
2024-07-26 05:56:33 +0300

    add:printcomb

---
+++ go-skills/printcomb/printcomb.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	PrintComb()
+}
+
+func PrintComb() {
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for h := j + 1; h <= '9'; h++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(h)
+				if i == '7' && j == '8' && h == '9' {
+					break
+				} else {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+
+		}
+	}
+}
